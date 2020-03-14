618770a59f0b4428724344895b024ac3ae5cf819
Pavel Treschanovsky <pavel@angtel.ru>
2020-03-14 10:36:08 +0300

    unix-processs: Simplifications and clarifications

--- lectures/lesson03-unix-processes/class-task-templates/catch-signals.c
+++ lectures/lesson03-unix-processes/class-task-templates/catch-signals.c
@@ -9,14 +9,16 @@
 #include <errno.h>
 #include <assert.h>
 
+#define FIXME 0
+
 static bool sigterm;
 static bool sigint;
 static bool sigchld;
-static bool sigsegv;
 static bool sigkill;
 
 static void signal_handler(int sig)
 {
+	/* ... */
 }
 
 int main(int argc, char *argv[])
@@ -26,14 +28,21 @@ int main(int argc, char *argv[])
 
 	/* Регистрация обработчика для SIGTERM, SIGINT, SIGKILL, SIGCHLD. */
 
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = FIXME;
+	sigaction(FIXME, &sa, NULL);
+
 	/* ... */
 
 	while (1) {
 		char buf[100];
 
 		ret = read(STDIN_FILENO, buf, sizeof(buf));
-
-		/* ... */
+		if (FIXME) {
+			printf("Interrupted by signal: "
+			       "term %d, int %d, chld %d, kill %d\n",
+			       sigterm, sigint, sigchld, sigkill);
+		}
 	}
 
 	return 0;
--- lectures/lesson03-unix-processes/class-task-templates/fork-process.c
+++ lectures/lesson03-unix-processes/class-task-templates/fork-process.c
@@ -8,13 +8,40 @@
 #include <errno.h>
 #include <assert.h>
 
+#define FIXME 0
+
 int main(int argc, char *argv[])
 {
 	pid_t pid;
 
-	/* В коде используются функции fork(), exit(), waitpid(), getpid(), а
-	 * также макросы WIFEXITED, WEXITSTATUS, WIFSIGNALED, WTERMSIG.
-	 */
+	printf("Parent PID: %d\n", getpid());
 
+	pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "Failed to fork: %s\n", strerror(errno));
+		return -1;
+	}
+	
+	if (FIXME) {
+		/* Дочерний процесс */
+
+		exit(FIXME);
+	} else if (FIXME) {
+		/* Родительский процесс */
+		int child_status;
+		pit_t terminated_pid;
+
+		terminated_pid = waitpid(FIXME, &child_status, FIXME);
+
+		/* Для работы с переменной child_status используются макросы
+		 * WIFEXITED, WEXITSTATUS, WIFSIGNALED, WTERMSIG.
+		 */
+		if (FIXME)
+			printf("Child %d exited with status %d\n",
+			       FIXME, FIXME);
+		else if (FIXME)
+			printf("Child %d was terminated by signal %d\n",
+			       FIXME, FIXME);
+	}
 	return 0;
 }
